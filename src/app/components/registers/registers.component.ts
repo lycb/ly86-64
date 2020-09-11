f8696843a0519d170bef1dd176092fc88e2af834
Jenny Ly <lycb@appstate.edu>
2020-09-11 17:33:28 -0400

    add hex string to register component

--- src/app/components/registers/registers.component.ts
+++ src/app/components/registers/registers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ParserService } from '../../services/parser/parser.service';
 import { RegisterService } from '../../services/register/register.service';
 import { Register } from '../../models/Register';
+import Long from 'long';
 
 @Component({
   selector: 'app-registers',
@@ -9,7 +10,7 @@ import { Register } from '../../models/Register';
   styleUrls: ['./registers.component.css']
 })
 export class RegistersComponent implements OnInit {
-	headers = ["name", "value"];
+	headers = ["name", "hex", "value"];
 	register: Register[];
   constructor(private parserService: ParserService, private registerService: RegisterService) { }
 
--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -14,12 +14,52 @@ export class RegisterService {
 	  for (let reg of REGISTERS) {
   		this.register.push({
   			name: reg,
-  			value: new Long(0,0)
+  			value: Long.ZERO,
+        hex: "0x0000000000000000"
   		});
 	  }
 	}
 
   getRegisters(): Register[] {
   	return this.register;
+  }
+
+  getValueByRegister(name: string): Long {
+    return this.register[this.matchRegisterNameToIndex(name)].value;
+  }
+
+  setValueByRegister(name: string, value: Long): void {
+    this.register[this.matchRegisterNameToIndex(name)].value = value;
+    var binaryNum = value.toNumber().toString(16);
+    this.register[this.matchRegisterNameToIndex(name)].hex = this.paddingHex(binaryNum, 16);
+
+  }
+
+  matchRegisterNameToIndex(name: string): number {
+    switch(name) {
+      case "RAX": return 0;
+      case "RCX": return 1;
+      case "RDX": return 2;
+      case "RBX": return 3;
+      case "RSP": return 4;
+      case "RBP": return 5;
+      case "RSI": return 6;
+      case "RDI": return 7;
+      case "R8": return 8;
+      case "R9": return 9;
+      case "R10": return 10;
+      case "R11": return 11;
+      case "R12": return 12;
+      case "R13": return 13;
+      case "R14": return 14;
+    }
+  }
+
+  paddingHex(num, width): string {
+    var result = num.toString(16);
+    while (result.length < width) {
+      result = '0' + result;
+    } 
+    return "0x" + result;
   }
 }
