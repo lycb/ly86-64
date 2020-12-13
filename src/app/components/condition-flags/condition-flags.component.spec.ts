61f5597667801c5d91fc1de716d8f01817ed072c
Jenny Ly <chaulyjenny@gmail.com>
2020-12-13 17:09:58 -0500

    upgrading all the packages because angular is confusing to upgrade

--- src/app/components/condition-flags/condition-flags.component.spec.ts
+++ src/app/components/condition-flags/condition-flags.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ConditionFlagsComponent } from './condition-flags.component';
 
@@ -6,7 +6,7 @@ describe('ConditionFlagsComponent', () => {
   let component: ConditionFlagsComponent;
   let fixture: ComponentFixture<ConditionFlagsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ConditionFlagsComponent ]
     })
