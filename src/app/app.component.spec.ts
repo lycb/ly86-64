a07453e7efd9728267790b5616ebe2e39221a3ad
Jenny Ly <lycb@appstate.edu>
2020-12-20 14:39:20 -0500

    getting started with testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'y86'`, () => {
+  it(`should have as title 'LY86-64'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('y86');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('y86 app is running!');
+    expect(app.title).toEqual('LY86-64');
   });
 });
