05eea2ae1efaf4917b7a316033f27bf5910e68d8
Jenny Ly <jennyly294@gmail.com>
2021-01-21 20:49:54 -0500

    change path for test build

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -124,7 +124,7 @@ export class ButtonsComponent implements OnInit {
     let filename = (<HTMLInputElement>document.getElementById("dropdown")).value;
     if (filename !== "upload" && filename !== "choose") {
       let txt;
-      let path = "assets/sample/yo_files/" + filename;
+      let path = "/assets/sample/yo_files/" + filename;
       var rawFile = new XMLHttpRequest();
       rawFile.open("GET", path, false);
       rawFile.onreadystatechange = function() {
