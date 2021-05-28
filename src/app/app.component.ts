43dbd3dd1c6f7111ac3ea131f7c959ef9a7deba2
Jenny Ly <jennyly294@gmail.com>
2021-05-28 19:07:27 -0400

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{Router, NavigationEnd} from '@angular/router';
 import { ClockCycleComponent } from './components/clock-cycle/clock-cycle.component';
 import { RegistersComponent } from './components/registers/registers.component';
 import { ControlComponent } from './components/control/control.component';
@@ -7,11 +8,22 @@ import { PipelineRegComponent } from './components/pipeline-reg/pipeline-reg.com
 import { ConditionFlagsComponent } from './components/condition-flags/condition-flags.component';
 import { ControlLogicComponent } from './components/control-logic/control-logic.component';
 
+declare let ga: Function;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'LY86-64';
+  constructor(public router: Router){   
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
 }
